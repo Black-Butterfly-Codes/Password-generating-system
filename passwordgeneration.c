02556b962a5f2550ff51a7be1988d5a70460b53b
Black-Butterfly-Codes <blackbutterflycodes@gmail.com>
2026-06-19 14:03:51 +0530

    Added account-based password assignment feature

--- passwordgeneration.c
+++ passwordgeneration.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main()
 {
     int num;
+    char account[50];
 
     printf("Welcome to the Password Generator\n");
+
+    printf("Enter account name: ");
+    fgets(account, sizeof(account), stdin);
+
+    account[strcspn(account, "\n")] = '\0';
+
     printf("Enter the length of the password: ");
     scanf("%d", &num);
 
@@ -36,6 +44,6 @@ int main()
     password[num] = '\0';
 
     printf("\nGenerated Password: %s\n", password);
+    printf("Assigned To: %s\n", account);
 
     return 0;
-}
