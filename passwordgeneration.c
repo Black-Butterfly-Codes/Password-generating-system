5cb2b07957ed065b42ea24dcdeb2b426e24bbca7
Black-Butterfly-Codes <blackbutterflycodes@gmail.com>
2026-06-20 14:03:30 +0530

    Add multiple account password generation

--- passwordgeneration.c
+++ passwordgeneration.c
@@ -5,46 +5,62 @@
 
 int main()
 {
-    int num;
-    char account[50];
+    int total;
 
     printf("Welcome to the Password Generator\n");
 
-    printf("Enter account name: ");
-    fgets(account, sizeof(account), stdin);
+    printf("How many accounts do you want to create? ");
+    scanf("%d", &total);
 
-    account[strcspn(account, "\n")] = '\0';
-
-    printf("Enter the length of the password: ");
-    scanf("%d", &num);
-
-    if(num <= 0)
-    {
-        printf("Invalid password length.\n");
-        return 1;
-    }
-
-    char characters[] =
-    "0123456789"
-    "abcdefghijklmnopqrstuvwxyz"
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "!@#$%^&*()-_=+[]{};:,.<>?/";
-
-    int size = sizeof(characters) - 1;
-
-    char password[num + 1];
+    getchar(); // consume newline left by scanf
 
     srand(time(NULL));
 
-    for(int i = 0; i < num; i++)
+    for(int j = 0; j < total; j++)
     {
-        password[i] = characters[rand() % size];
+        int num;
+        char account[50];
+
+        printf("\n==============================\n");
+        printf("Account %d\n", j + 1);
+        printf("==============================\n");
+
+        printf("Enter account name: ");
+        fgets(account, sizeof(account), stdin);
+
+        account[strcspn(account, "\n")] = '\0';
+
+        printf("Enter the length of the password: ");
+        scanf("%d", &num);
+
+        getchar(); // consume newline
+
+        if(num <= 0)
+        {
+            printf("Invalid password length.\n");
+            continue;
+        }
+
+        char characters[] =
+        "0123456789"
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "!@#$%^&*()-_=+[]{};:,.<>?/";
+
+        int size = sizeof(characters) - 1;
+
+        char password[num + 1];
+
+        for(int i = 0; i < num; i++)
+        {
+            password[i] = characters[rand() % size];
+        }
+
+        password[num] = '\0';
+
+        printf("\nGenerated Password: %s\n", password);
+        printf("Assigned To: %s\n", account);
     }
-
-    password[num] = '\0';
-
-    printf("\nGenerated Password: %s\n", password);
-    printf("Assigned To: %s\n", account);
 
     return 0;
 }
