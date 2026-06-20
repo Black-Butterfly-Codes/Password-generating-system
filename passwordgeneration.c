c2e7cdc9f30f00f0a0c9955fc264cef5746846a9
Black-Butterfly-Codes <blackbutterflycodes@gmail.com>
2026-06-20 14:23:04 +0530

    Added menu-driven password manager with file handling

--- passwordgeneration.c
+++ passwordgeneration.c
@@ -5,62 +5,117 @@
 
 int main()
 {
-    int total;
+    int choice;
 
-    printf("Welcome to the Password Generator\n");
-
-    printf("How many accounts do you want to create? ");
-    scanf("%d", &total);
-
-    getchar(); // consume newline left by scanf
-
-    srand(time(NULL));
-
-    for(int j = 0; j < total; j++)
+    do
     {
-        int num;
-        char account[50];
+        printf("\n==================================\n");
+        printf("       PASSWORD MANAGER\n");
+        printf("==================================\n");
+        printf("1. Create New Password\n");
+        printf("2. View Saved Passwords\n");
+        printf("3. Exit\n");
+        printf("==================================\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        getchar();
 
-        printf("\n==============================\n");
-        printf("Account %d\n", j + 1);
-        printf("==============================\n");
-
-        printf("Enter account name: ");
-        fgets(account, sizeof(account), stdin);
-
-        account[strcspn(account, "\n")] = '\0';
-
-        printf("Enter the length of the password: ");
-        scanf("%d", &num);
-
-        getchar(); // consume newline
-
-        if(num <= 0)
+        switch(choice)
         {
-            printf("Invalid password length.\n");
-            continue;
+            case 1:
+            {
+                int num;
+                char account[50];
+
+                printf("\nEnter account name: ");
+                fgets(account, sizeof(account), stdin);
+                account[strcspn(account, "\n")] = '\0';
+
+                printf("Enter password length: ");
+                scanf("%d", &num);
+                getchar();
+
+                if(num <= 0)
+                {
+                    printf("Invalid password length.\n");
+                    break;
+                }
+
+                char characters[] =
+                "0123456789"
+                "abcdefghijklmnopqrstuvwxyz"
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                "!@#$%^&*()-_=+[]{};:,.<>?/";
+
+                int size = sizeof(characters) - 1;
+
+                char password[num + 1];
+
+                srand(time(NULL));
+
+                for(int i = 0; i < num; i++)
+                {
+                    password[i] = characters[rand() % size];
+                }
+
+                password[num] = '\0';
+
+                FILE *fp = fopen("passwords.txt", "a");
+
+                if(fp == NULL)
+                {
+                    printf("Error opening file.\n");
+                    break;
+                }
+
+                fprintf(fp, "%s | %s\n", account, password);
+                fclose(fp);
+
+                printf("\nGenerated Password: %s\n", password);
+                printf("Assigned To: %s\n", account);
+                printf("Password saved successfully!\n");
+
+                break;
+            }
+
+            case 2:
+            {
+                FILE *fp = fopen("passwords.txt", "r");
+
+                if(fp == NULL)
+                {
+                    printf("\nNo saved passwords found.\n");
+                    break;
+                }
+
+                char line[200];
+
+                printf("\n========== SAVED PASSWORDS ==========\n");
+
+                while(fgets(line, sizeof(line), fp) != NULL)
+                {
+                    printf("%s", line);
+                }
+
+                printf("=====================================\n");
+
+                fclose(fp);
+                break;
+            }
+
+            case 3:
+            {
+                printf("\nThank you for using Password Manager.\n");
+                break;
+            }
+
+            default:
+            {
+                printf("\nInvalid choice.\n");
+            }
         }
 
-        char characters[] =
-        "0123456789"
-        "abcdefghijklmnopqrstuvwxyz"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "!@#$%^&*()-_=+[]{};:,.<>?/";
-
-        int size = sizeof(characters) - 1;
-
-        char password[num + 1];
-
-        for(int i = 0; i < num; i++)
-        {
-            password[i] = characters[rand() % size];
-        }
-
-        password[num] = '\0';
-
-        printf("\nGenerated Password: %s\n", password);
-        printf("Assigned To: %s\n", account);
-    }
+    } while(choice != 3);
 
     return 0;
 }
